4ebb283125cb4b585b11b703e9dcd436af038836
Fumaz <mail@fumaz.dev>
2024-08-15 21:47:03 +0200

    fixes

--- owo/start.sh
+++ owo/start.sh
@@ -4,4 +4,5 @@ python3.11 /usr/src/app/script.py
 wget $DATASET_URL -O dataset.zip
 unzip -o dataset.zip -d /workspace/SimpleTuner/dataset
 
-bash /workspace/SimpleTuner/train.sh
+cd /workspace/SimpleTuner
+bash train.sh
