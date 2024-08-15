a083c83a3b76f09b4ebe2ae01340a808a5889c99
Fumaz <mail@fumaz.dev>
2024-08-15 23:12:39 +0200

    fix

--- owo/start.sh
+++ owo/start.sh
@@ -4,8 +4,11 @@ python3.11 /usr/src/app/script.py
 wget $DATASET_URL -O dataset.zip
 mkdir -p /usr/src/app/output/dataset
 unzip -o dataset.zip -d /usr/src/app/output/dataset
+rm -rf /usr/src/app/output/dataset/__MACOSX
+ls -la /usr/src/app/output/dataset
 mkdir -p /workspace/SimpleTuner/dataset
 unzip -o dataset.zip -d /workspace/SimpleTuner/dataset
+rm -rf /workspace/SimpleTuner/dataset/__MACOSX
 
 cd /workspace/SimpleTuner
 export HF_HOME=/workspace/hf_home
